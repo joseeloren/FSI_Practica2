7b90fe0d3d7df3fd02a2d0bd3a8e7f9c5076fc8e
Jose <isleo11@gmail.com>
2017-04-04 00:04:42 +0100

    First commit

---
+++ nn_mnist.py
@@ -0,0 +1,41 @@
+import gzip
+import cPickle
+
+import tensorflow as tf
+import numpy as np
+
+
+# Translate a list of labels into an array of 0's and one 1.
+# i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0]
+def one_hot(x, n):
+    """
+    :param x: label (int)
+    :param n: number of bits
+    :return: one hot code
+    """
+    if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
+
+
+f = gzip.open('mnist.pkl.gz', 'rb')
+train_set, valid_set, test_set = cPickle.load(f)
+f.close()
+
+train_x, train_y = train_set
+
+
+# ---------------- Visualizing some element of the MNIST dataset --------------
+
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+plt.imshow(train_x[57].reshape((28, 28)), cmap=cm.Greys_r)
+plt.show()  # Let's see a sample
+print train_y[57]
+
+
+# TODO: the neural net!!
